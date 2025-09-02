995c0c71eb5275a2501b8bdefe9d9ad0abbdf573
tsheporamantso <tshepo.ramantso@outlook.com>
2025-09-02 12:01:21 +0200

    Fetch data from API

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,4 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const url = "https://www.course-api.com/react-useReducer-cart-project";
 
 const initialState = {
   cartItems: [],
@@ -6,10 +9,37 @@ const initialState = {
   total: 0,
   isLoading: true,
 };
+export const getCartItems = createAsyncThunk("getCartItems", async () => {
+  try {
+    const { data } = await axios.get(url);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.cartItems = action.payload;
+        console.log(state.cartItems);
+      })
+      .addCase(getCartItems.rejected, (state) => {
+        state.isLoading = false;
+      });
+  },
 });
+
+// console.log(cartSlice);
+
+// export const {} = cartSlice.actions;
 
 export default cartSlice.reducer;
