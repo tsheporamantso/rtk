b3e5c688d5eb3af2b28a14c8c428e0a4ada82ae9
tsheporamantso <tshepo.ramantso@outlook.com>
2025-09-02 13:31:22 +0200

    Dispatched clear Item and remove Item  actions

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import CartItem from "./CartItem";
 import { useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
 
 const CartContainer = () => {
   const { cartItems, amount, total } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
 
   if (amount < 1) {
     return (
@@ -33,7 +36,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          clear cart
+        </button>
       </footer>
     </section>
   );
